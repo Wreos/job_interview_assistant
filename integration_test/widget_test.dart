1c37be323cd21a40f9c86d6b3d53efc644d5f191
Aleksandr Lozhkovoi <aleksandr.lozhkovoi@auto1.com>
2023-01-29 22:20:43 +0100

    Updated icons

--- integration_test/widget_test.dart
+++ integration_test/widget_test.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:job_interview_assistant/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:job_interview_assistant/main.dart';
 
 void main() {
   testWidgets('Interview List smoke test', (WidgetTester tester) async {
